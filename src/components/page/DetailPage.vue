fe74eb35347fdc03dafd1539e365ef0d6e452c90
nakao107107 <imakeanewstory@gmail.com>
2019-02-16 15:06:50 +0900

    詳細ページでのサークル詳細の再取得機能の作成

--- src/components/page/DetailPage.vue
+++ src/components/page/DetailPage.vue
@@ -1,12 +1,33 @@
 <template>
   <div>
     detailpage
-    {{$route.params.id}}
+    {{circleInfo.name}}
   </div>
 </template>
-
+ya
 <script>
 export default {
+  data(){
+    return {
+      circleInfo: {}
+    }
+  },
+  created(){
+    var id = this.$route.params.id;
+    
+    var url = `http://tk2-215-17314.vs.sakura.ne.jp:3000/circles/${id}.json`;
+     
+    this.$axios.get(url)
+    
+    .catch(error => {
+
+      return error
+
+    }).then(response =>{
+      this.circleInfo = response.data;
+      console.log(this.circleInfo);
+    });
+  },
   
 }
 </script>
