272b8ccfb1fd68cee93f0bdcffbfb090d1c7864b
nakao107107 <imakeanewstory@gmail.com>
2019-02-16 13:40:27 +0900

    index pageでのtab切り替え機能の作成

--- src/components/page/IndexPage.vue
+++ src/components/page/IndexPage.vue
@@ -1,12 +1,36 @@
 <template>
   <div>
-    indexpage
+    <div class="mode-tab">
+      <div @click="changeMode('index')">
+        <p>サークル一覧</p>
+      </div>
+      <div @click="changeMode('like')">
+        <p>お気に入り</p>
+      </div>
+      <index v-if="mode=='index'"/>
+      <like v-if="mode=='like'"/>  
+    </div>
   </div>
 </template>
 
 <script>
+import Index from '@/components/index/Index';
+import Like from '@/components/index/Like';
 export default {
-  
+  data(){
+    return {
+      mode: ""
+    }
+  },
+  methods: {
+    changeMode(mode){
+      this.mode = mode
+    }
+  },
+  components: {
+      'index': Index,
+      'like': Like
+  }
 }
 </script>
 
